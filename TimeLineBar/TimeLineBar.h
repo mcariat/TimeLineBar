adaabe5fa830bb3d3ed28c644f7dda9462da2529
Matthieu Cariat <matthieu.cariat@imerir.com>
2018-01-03 10:58:25 +0100

    Initial Commit

---
+++ TimeLineBar/TimeLineBar.h
@@ -0,0 +1,19 @@
+//
+//  TimeLineBar.h
+//  TimeLineBar
+//
+//  Created by Cariat Matthieu on 03/01/2018.
+//  Copyright © 2018 Cariat Matthieu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TimeLineBar.
+FOUNDATION_EXPORT double TimeLineBarVersionNumber;
+
+//! Project version string for TimeLineBar.
+FOUNDATION_EXPORT const unsigned char TimeLineBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TimeLineBar/PublicHeader.h>
+
+
